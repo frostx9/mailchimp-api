b0449f43b5e63ee43e4bbd2d32475c203a1a9ed1
Saumya Som <saumyasom92@gmail.com>
2023-10-02 21:11:06 +0530

    To Check Mail Chimp Is Working Or Not

--- index.js
+++ index.js
@@ -6,3 +6,10 @@ mailchimp.setConfig({
   server: "<Your Server Location>" // Ex :  us5
 })
 
+// To Check Mail Chimp Is Working Or Not
+const run = async () => {
+  const response = await mailchimp.ping.get();
+  console.log(response);
+}
+
+// run();
